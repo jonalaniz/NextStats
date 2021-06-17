a7779491d69a3359f8bd6e2407c202c042949af1
Jon Alaniz <me@jonalaniz.com>
2021-06-17 04:21:29 -0500

    Cleaned lint (info models

--- NextStats/Controllers/InfoViewController.swift
+++ NextStats/Controllers/InfoViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class InfoViewController: UIViewController {
     weak var coordinator: InfoCoordinator?
-    
+
     let infoModel = InfoModel()
-    let tableView = UITableView(frame: CGRect.zero , style: .insetGrouped)
+    let tableView = UITableView(frame: CGRect.zero, style: .insetGrouped)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,35 +24,35 @@ class InfoViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = false
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissController))
         title = "Info"
-        
+
         // Setup View
         view.backgroundColor = .systemBackground
-        
+
         // Connect the TableView to ViewController
         tableView.delegate = self
         tableView.dataSource = self
-        
+
         // Setup TableViewHeader
         tableView.tableHeaderView = HeaderView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         view.addSubview(tableView)
-        
+
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.topAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
-            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor)
         ])
     }
-    
+
     @objc func dismissController() {
         coordinator?.didFinish()
         dismiss(animated: true, completion: nil)
     }
 }
 
-/// MARK: TableView Functions
+// MARK: TableView Functions
 extension InfoViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return infoModel.numberOfSections()
@@ -61,14 +61,14 @@ extension InfoViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return infoModel.numberOfRows(in: section)
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .value1, reuseIdentifier: "Cell")
-        
+
         if indexPath.section == 2 {
             cell.accessoryType = .disclosureIndicator
         }
-        
+
         cell.textLabel?.text = infoModel.titleLabelFor(row: indexPath.row, section: indexPath.section)
         cell.detailTextLabel?.text = infoModel.detailLabelFor(row: indexPath.row, section: indexPath.section)
 
@@ -78,14 +78,14 @@ extension InfoViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return infoModel.title(for: section)
     }
-    
+
     func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
         return infoModel.footer(for: section)
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        
+
         switch indexPath.section {
         case 2:
             // Show proper license information
