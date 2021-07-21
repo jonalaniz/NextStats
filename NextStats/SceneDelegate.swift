67994e5310a0cd9af5e22976675eb1704f36582b
Jon Alaniz <me@jonalaniz.com>
2021-07-21 06:09:00 -0500

    Refactored `SceneDelegate.swift`

--- NextStats/SceneDelegate.swift
+++ NextStats/SceneDelegate.swift
@@ -15,8 +15,36 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, UISplitViewControllerDe
     var toolbarDelegate = ToolbarDelegate()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Grab the windowScene
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+        guard let windowScene = scene as? UIWindowScene else { fatalError() }
+        window = UIWindow(windowScene: windowScene)
+
+        configureSplitViewController()
+
+        #if targetEnvironment(macCatalyst)
+        // Set min and max windows size
+        UIApplication.shared.connectedScenes.compactMap { $0 as? UIWindowScene }.forEach { windowScene in
+            let minimumSize = CGSize(width: 800, height: 600)
+            let maximumSize = CGSize(width: 1024, height: 768)
+
+            windowScene.sizeRestrictions?.minimumSize = minimumSize
+            windowScene.sizeRestrictions?.maximumSize = maximumSize
+        }
+
+        // Create toolbar
+        let toolbar = NSToolbar(identifier: "main")
+        toolbar.delegate = toolbarDelegate
+        toolbar.displayMode = .iconOnly
+
+        // Remove titlebar and set toolbar
+        if let titlebar = windowScene.titlebar {
+            titlebar.toolbar = toolbar
+            titlebar.toolbarStyle = .automatic
+        }
+        #endif
+    }
+
+    private func configureSplitViewController() {
+        guard let window = window else { fatalError() }
 
         // Initialize a SplitViewController and Coordinator
         let splitViewController = UISplitViewController()
@@ -30,67 +58,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, UISplitViewControllerDe
         splitViewController.preferredDisplayMode = UISplitViewController.DisplayMode.oneBesideSecondary
 
         // Set the window to the SplitViewController
-        let window = UIWindow(windowScene: windowScene)
         window.rootViewController = splitViewController
         window.makeKeyAndVisible()
-        self.window = window
-
-        #if targetEnvironment(macCatalyst)
-        // Set minimum windows size
-        UIApplication.shared.connectedScenes.compactMap { $0 as? UIWindowScene }.forEach { windowScene in
-            let size = CGSize(width: 800, height: 600)
-
-            windowScene.sizeRestrictions?.minimumSize = size
-        }
-
-        // Create toolbar
-        guard let windowScene = scene as? UIWindowScene else { return }
-
-        let toolbar = NSToolbar(identifier: "main")
-        toolbar.delegate = toolbarDelegate
-        toolbar.displayMode = .iconOnly
-
-        // Remove titlebar and set toolbar
-        if let titlebar = windowScene.titlebar {
-            titlebar.toolbar = toolbar
-            titlebar.toolbarStyle = .automatic
-        }
-        #endif
-
         window.tintColor = .themeColor
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-
-    }
-
-    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController :UIViewController, onto primaryViewController:UIViewController) -> Bool {
-
+    func splitViewController(_ splitViewController: UISplitViewController,
+                             collapseSecondary secondaryViewController: UIViewController,
+                             onto primaryViewController: UIViewController) -> Bool {
         return true
     }
 
