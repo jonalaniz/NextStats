ff76d73241fcf24296de0c67645e97a3a4947828
Jon Alaniz <me@jonalaniz.com>
2021-08-01 20:22:17 -0500

    Added UserViewController and User model

---
+++ NextStats/Main/User Managment/UserViewController.swift
@@ -0,0 +1,22 @@
+//
+//  UserViewController.swift
+//  UserViewController
+//
+//  Created by Jon Alaniz on 7/31/21.
+//  Copyright © 2021 Jon Alaniz. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+    var userID: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    private func setupView() {
+        // Do setup here
+    }
+}
