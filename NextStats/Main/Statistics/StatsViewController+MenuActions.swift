8daa9226c185dd8832eab3b76efc388b1a78b5cb
Jon Alaniz <me@jonalaniz.com>
2022-02-04 02:19:48 -0600

    Added UIActionMenu Stubs

---
+++ NextStats/Main/Statistics/StatsViewController+MenuActions.swift
@@ -0,0 +1,23 @@
+//
+//  ServerMenu.swift
+//  NextStats
+//
+//  Created by Jon Alaniz on 2/4/22.
+//  Copyright © 2022 Jon Alaniz. All rights reserved.
+//
+
+import UIKit
+
+extension StatsViewController {
+    @objc func renameServer(action: UIAlertAction) {
+        print("Rename Server")
+    }
+
+    @objc func refreshIcon(action: UIAlertAction) {
+        print("Refresh Icon")
+    }
+
+    @objc func delete(action: UIAlertAction) {
+        print("Delete Server")
+    }
+}
