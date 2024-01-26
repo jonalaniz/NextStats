efecd08944257ccb34e1ff54e6d6bc0e04909718
Jon Alaniz <jonalaniz@gmail.com>
2024-01-26 01:53:10 -0600

    Inital UserCellModel

---
+++ NextStats/Nextcloud API/Users/UserCellModel.swift
@@ -0,0 +1,15 @@
+//
+//  UserCellModel.swift
+//  NextStats
+//
+//  Created by Jon Alaniz on 10/15/23.
+//  Copyright © 2023 Jon Alaniz. All rights reserved.
+//
+
+import Foundation
+
+struct UserCellModel {
+    let userID: String
+    let displayName: String
+    let email: String
+}
