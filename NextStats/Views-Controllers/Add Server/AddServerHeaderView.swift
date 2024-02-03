c69f52caa631f03f31fb6d764fbae88f853b7fb6
Jon Alaniz <jonalaniz@gmail.com>
2024-02-03 00:30:17 -0600

    Fix iPad mini UI

--- NextStats/Views-Controllers/Add Server/AddServerHeaderView.swift
+++ NextStats/Views-Controllers/Add Server/AddServerHeaderView.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class AddServerHeaderView: UIView {
+    let padding: CGFloat = {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            return 200
+        } else {
+            return 50
+        }
+    }()
+
     let imageView: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "nextcloud-drive-connect"))
         imageView.contentMode = .scaleAspectFit
@@ -36,7 +44,7 @@ class AddServerHeaderView: UIView {
     }()
 
     init() {
-        super.init(frame: CGRect(x: 0, y: 0, width: 700, height: 200))
+        super.init(frame: CGRect(x: 0, y: 0, width: 700, height: 140))
         setupView()
     }
 
@@ -55,12 +63,12 @@ class AddServerHeaderView: UIView {
 
         NSLayoutConstraint.activate([
             imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
-            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 40),
-            imageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -40),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
             activityIndicatior.centerXAnchor.constraint(equalTo: centerXAnchor),
             activityIndicatior.centerYAnchor.constraint(equalTo: bottomAnchor, constant: -30),
-            statusLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 40),
-            statusLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -40),
+            statusLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50),
+            statusLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -50),
             statusLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12),
             statusLabel.heightAnchor.constraint(equalToConstant: 14)
         ])
