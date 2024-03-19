c21568ce24e8261c58095450bcb0fc29ad8a2925
Jon Alaniz <jonalaniz@gmail.com>
2024-03-19 14:49:58 -0500

    Added groups endpoint

--- NextStats/Nextcloud API/Networking/Endpoints.swift
+++ NextStats/Nextcloud API/Networking/Endpoints.swift
@@ -9,10 +9,10 @@ import Foundation
 
 enum Endpoints: String {
     case appPassword = "/ocs/v2.php/core/apppassword"
+    case groups = "ocs/v1.php/cloud/groups"
+    case info = "/ocs/v2.php/apps/serverinfo/api/v1/info"
     case login = "/index.php/login/v2"
     case logo = "/index.php/apps/theming/image/logo"
-    case info = "/ocs/v2.php/apps/serverinfo/api/v1/info"
-    case users = "/ocs/v1.php/cloud/users"
-    case user = "/ocs/v1.php/cloud/users/"
     case settings = "/index.php/apps/theming/manifest/settings"
+    case users = "/ocs/v1.php/cloud/users"
 }
