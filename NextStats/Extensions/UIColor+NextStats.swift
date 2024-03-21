743eae20be895ac21f287165d540aae63c8de527
Jon Alaniz <jonalaniz@gmail.com>
2024-03-21 02:08:38 -0500

    Icons & Theme

--- NextStats/Extensions/UIColor+NextStats.swift
+++ NextStats/Extensions/UIColor+NextStats.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 extension UIColor {
-    static let themeColor = UIColor(red: 142 / 255, green: 154 / 255, blue: 255 / 255, alpha: 1.0)
+//    static let themeColor = UIColor(red: 142 / 255, green: 154 / 255, blue: 255 / 255, alpha: 1.0)
+    static let themeColor = UIColor(named: "themeColor")!
+    static let button = UIColor(named: "button")!
     static let statusLabelRed = UIColor(red: 255/255, green: 42/255, blue: 85/255, alpha: 1)
 }
