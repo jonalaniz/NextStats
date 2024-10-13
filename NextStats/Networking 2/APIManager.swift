d221a258859f03a0d260bc0c33785d7c564f6330
Jon Alaniz <jonalaniz@gmail.com>
2024-10-13 17:45:43 -0500

    linting

--- NextStats/Networking 2/APIManager.swift
+++ NextStats/Networking 2/APIManager.swift
@@ -9,38 +9,40 @@ import Foundation
 
 final class APIManager: Managable {
     public internal(set) var session = URLSession.shared
-    
+
     static let shared: Managable = APIManager()
-    
+
     private init() {}
-    
+
     func request<T>(url: URL,
                     httpMethod: ServiceMethod,
                     body: Data?,
-                    headers: [String : String]?,
+                    headers: [String: String]?,
                     expectingReturnType: T.Type
-    ) async throws -> T where T : Decodable, T : Encodable {
+    ) async throws -> T where T: Decodable, T: Encodable {
         var request = URLRequest(url: url)
         request.httpMethod = httpMethod.rawValue
-        
+
         if let body = body, httpMethod != .get {
             request.httpBody = body
         }
 
-        request.addHeaders(from: headers)
+        if let unwrappedHeaders = headers {
+            request.addHeaders(from: unwrappedHeaders)
+        }
         
-        
-        
+        request.setUserAgent()
+
         return try await self.responseHandler(session.data(for: request))
     }
-    
+
     func responseHandler<T: Codable>(_ dataWithResponse: (data: Data, response: URLResponse)) async throws -> T {
         guard let response = dataWithResponse.response as? HTTPURLResponse else {
             throw APIManagerError.conversionFailedToHTTPURLResponse
         }
-        
+
         try response.statusCodeChecker()
-                
+
         return try JSONDecoder().decode(T.self, from: dataWithResponse.data)
     }
 }
