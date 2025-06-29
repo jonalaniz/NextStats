de7ee499776a9dd8a09a2b3942d5343c9c1a1fb3
Jon Alaniz <jonalaniz@gmail.com>
2025-06-29 08:02:34 -0500

    Modified TableRow to remove unused method and add color

--- NextStats/iOS/TableView/Models/TableRow.swift
+++ NextStats/iOS/TableView/Models/TableRow.swift
@@ -10,25 +10,27 @@ import UIKit
 
 struct TableRow {
     let title: String
+    let titleColor: UIColor
     let secondaryText: String?
-    let style: UITableViewCell.CellStyle
+    let secondaryColor: UIColor
     let accessoryType: UITableViewCell.AccessoryType
-    let custom: Bool
     let action: (() -> Void)?
     let progressData: ProgressCellData?
 
     init(
         title: String,
+        titleColor: UIColor = .theme,
         secondaryText: String?,
+        secondaryColor: UIColor = .secondaryLabel,
         style: UITableViewCell.CellStyle = .value1,
         accessoryType: UITableViewCell.AccessoryType = .none,
         action: (() -> Void)? = nil
     ) {
         self.title = title
+        self.titleColor = titleColor
         self.secondaryText = secondaryText
-        self.style = style
+        self.secondaryColor = secondaryColor
         self.accessoryType = accessoryType
-        self.custom = false
         self.action = action
         self.progressData = nil
     }
@@ -38,10 +40,10 @@ struct TableRow {
         progressData: ProgressCellData
     ) {
         self.title = title
+        self.titleColor = .theme
         self.secondaryText = nil
-        self.style = .default
+        self.secondaryColor = .secondaryLabel
         self.accessoryType = .none
-        self.custom = true
         self.action = nil
         self.progressData = progressData
     }
