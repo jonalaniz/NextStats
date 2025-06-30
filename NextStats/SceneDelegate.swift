d01bc85abac357e7b731d95a362f302d8f7a74c3
Jon Alaniz <jonalaniz@gmail.com>
2025-06-30 08:14:05 -0500

    Removed swiftlint:disable all

--- NextStats/SceneDelegate.swift
+++ NextStats/SceneDelegate.swift
@@ -8,21 +8,27 @@
 
 import UIKit
 
-// swiftlint:disable all
 class SceneDelegate: UIResponder, UIWindowSceneDelegate, UISplitViewControllerDelegate {
     var coordinator: MainCoordinator?
     var window: UIWindow?
     var toolbarDelegate = ToolbarDelegate()
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = scene as? UIWindowScene else { fatalError() }
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene
+        else { fatalError() }
         window = UIWindow(windowScene: windowScene)
 
         configureSplitViewController()
 
         #if targetEnvironment(macCatalyst)
         // Set min and max windows size
-        UIApplication.shared.connectedScenes.compactMap { $0 as? UIWindowScene }.forEach { windowScene in
+        UIApplication.shared.connectedScenes.compactMap {
+            $0 as? UIWindowScene
+        }.forEach { windowScene in
             let minimumSize = CGSize(width: 800, height: 600)
             let maximumSize = CGSize(width: 1024, height: 768)
 
@@ -50,31 +56,32 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, UISplitViewControllerDe
         guard let window = window else { fatalError() }
 
         // Initialize a SplitViewController and Coordinator
-        let splitViewController: UISplitViewController
-        
+        let splitVC: UISplitViewController
+
         #if targetEnvironment(macCatalyst)
-        splitViewController = UISplitViewController(style: .doubleColumn)
+        splitVC = UISplitViewController(style: .doubleColumn)
         #else
-        splitViewController = UISplitViewController()
+        splitVC = UISplitViewController()
         #endif
-        
-        coordinator = MainCoordinator(splitViewController: splitViewController)
+
+        coordinator = MainCoordinator(splitViewController: splitVC)
         coordinator?.start()
 
         // Setup our SplitViewController
-        splitViewController.preferredDisplayMode = UISplitViewController.DisplayMode.oneBesideSecondary
-        splitViewController.primaryBackgroundStyle = .sidebar
-        splitViewController.delegate = self
+        splitVC.preferredDisplayMode = .oneBesideSecondary
+        splitVC.primaryBackgroundStyle = .sidebar
+        splitVC.delegate = self
 
         // Set the window to the SplitViewController
-        window.rootViewController = splitViewController
+        window.rootViewController = splitVC
         window.makeKeyAndVisible()
         window.tintColor = .theme
     }
 
-    func splitViewController(_ splitViewController: UISplitViewController,
-                             collapseSecondary secondaryViewController: UIViewController,
-                             onto primaryViewController: UIViewController) -> Bool {
+    func splitViewController(
+        _ splitViewController: UISplitViewController,
+        collapseSecondary secondaryViewController: UIViewController,
+        onto primaryViewController: UIViewController) -> Bool {
         return true
     }
 
