d7cef25dd1820399e3102b1c5aeb832bbbd12093
Jon Alaniz <jonalaniz@gmail.com>
2025-09-11 15:22:36 -0500

    Updated padding and removed unused class.

--- NextStats/Interface/Info/InfoHeaderView.swift
+++ NextStats/Interface/Info/InfoHeaderView.swift
@@ -16,7 +16,7 @@ class InfoHeaderView: UIView {
         stackView.axis = .vertical
         stackView.alignment = .center
         stackView.distribution = .fill
-        stackView.spacing = 6
+        stackView.spacing = 8
 
         return stackView
     }()
@@ -76,97 +76,6 @@ class InfoHeaderView: UIView {
             mainStackView.leftAnchor.constraint(equalTo: leftAnchor, constant: 16),
             mainStackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -16),
             imageView.heightAnchor.constraint(equalToConstant: 180)
-        ])
-    }
-}
-
-class InfoHeaderViewX: UIView {
-    let mainStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .horizontal
-        stackView.alignment = .center
-        stackView.distribution = .fill
-
-        return stackView
-    }()
-
-    let labelStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.alignment = .leading
-
-        return stackView
-    }()
-
-    let imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.image = AppIcon.normal.image
-        imageView.contentMode = .scaleAspectFit
-
-        return imageView
-    }()
-
-    let appNameLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = .preferredFont(forTextStyle: .largeTitle)
-        label.textColor = .theme
-        label.text = "NextStats"
-
-        return label
-    }()
-
-    let versionLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = .preferredFont(forTextStyle: .headline)
-        label.textColor = .label
-        label.text = UIApplication.appVersion
-
-        return label
-    }()
-
-    let createdByLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = .preferredFont(forTextStyle: .callout)
-        label.textColor = .secondaryLabel
-        label.text = "Created by Jon Alaniz"
-
-        return label
-    }()
-
-    init() {
-        super.init(frame: CGRect(x: 0, y: 0, width: 300, height: 140))
-        setupView()
-    }
-
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setupView()
-    }
-
-    private func setupView() {
-        labelStackView.addArrangedSubview(appNameLabel)
-        labelStackView.addArrangedSubview(versionLabel)
-        labelStackView.addArrangedSubview(createdByLabel)
-
-        mainStackView.addArrangedSubview(imageView)
-        mainStackView.addArrangedSubview(labelStackView)
-
-        addSubview(mainStackView)
-
-        NSLayoutConstraint.activate([
-            imageView.widthAnchor.constraint(equalToConstant: 120),
-            imageView.heightAnchor.constraint(equalToConstant: 120),
-
-            mainStackView.topAnchor.constraint(equalTo: topAnchor, constant: 20),
-            mainStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            mainStackView.leftAnchor.constraint(equalTo: leftAnchor, constant: 10),
-            mainStackView.rightAnchor.constraint(equalTo: rightAnchor)
         ])
     }
 }
